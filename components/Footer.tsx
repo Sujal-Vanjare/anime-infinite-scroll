b7edad0c375db5eebfd328265727e95fc3d43907
Sujal-Vanjare <141014768+Sujal-Vanjare@users.noreply.github.com>
2024-02-13 01:11:08 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#161921]">
+    <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap ">
       <p className="text-base font-bold text-white">@2023 EpicAnimeVault</p>
       <Image
         src="/logo.png"
