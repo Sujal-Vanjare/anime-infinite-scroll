fea60091bba104c2cc89b7a8d66286f4425eac05
Sujal Vanjare <141014768+Sujal-Vanjare@users.noreply.github.com>
2024-12-03 15:28:06 +0530

    added new icons

---
+++ app/manifest.ts
@@ -0,0 +1,37 @@
+import { siteConfig } from "@/config/site";
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: siteConfig.name,
+    short_name: siteConfig.shortName,
+    description: siteConfig.description,
+    start_url: "/",
+    display: "standalone",
+    background_color: "#191919",
+    theme_color: "#191919",
+    icons: [
+      {
+        src: "/favicon-16x16.png",
+        sizes: "16x16",
+        type: "image/png",
+      },
+      {
+        src: "/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+
+      {
+        src: "/favicon-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/favicon-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  };
+}
